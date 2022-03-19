96575f6794386fcbba8fafec35c34338c5ce7c85
CarlosCastillo123 <ccastillo0098476@gmail.com>
2022-03-19 12:43:11 -0700

    modified reptile.js

--- models/Reptile.js
+++ models/Reptile.js
@@ -49,10 +49,6 @@ Reptile.init(
       allowNull: false,
       defaultValue: DataTypes.NOW,
     },
-    needed_funding: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-    },
     user_id: {
       type: DataTypes.INTEGER,
       references: {
