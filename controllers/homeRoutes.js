b93dfa2e281e0e9f24383a8ad1f43d6bce83bd79
Niccolo Eck <nicco.eck@gmail.com>
2022-03-22 21:10:48 -0700

    Added routes to get routes to my homeRoutes.js

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,61 @@
+const router = require('express').Router();
+const { format } = require('path');
+const { Animal, User } = require('../models')
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        const animalData = await Animal.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+        const animals = animalData.map((animal) => animal.get({ plain: true }));
+
+        res.render('homepage', {
+            animals,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+router.get('/animal/:id', async (req, res) => {
+    try {
+        const animalData = await Animal.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+        const animal = animalData.get({ plain: true });
+
+        res.render('animal', {
+            ...animal,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/homepage');
+        return;
+    }
+
+    res.render('login');
+});
+
+module.exports = router;
