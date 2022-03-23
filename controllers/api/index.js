69808349176d54cc507331576aae9a1db9028598
JayMoses01 <jayrmoses@gmail.com>
2022-03-22 19:25:28 -0700

    Updating animalRoutes.js and index.js

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const animalRoutes = require('./animalRoutes');
+
+router.use('/users', userRoutes);
+router.use('/animals', animalRoutes);
+
+module.exports = router;
