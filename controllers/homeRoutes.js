d900634755f9fee61e037b82e2b2970510e288c3
Niccolo Eck <nicco.eck@gmail.com>
2022-03-22 21:47:26 -0700

    fixed my login redirect

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -51,7 +51,7 @@ router.get('/animal/:id', async (req, res) => {
 
 router.get('/login', (req, res) => {
     if (req.session.logged_in) {
-        res.redirect('/homepage');
+        res.redirect('/');
         return;
     }
 
