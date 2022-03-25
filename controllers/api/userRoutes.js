ff67b623a9076661f717c38e1b536ff89722a9ba
JayMoses01 <jayrmoses@gmail.com>
2022-03-24 21:31:27 -0700

    Adding signup feature

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -58,4 +58,19 @@ router.post('/logout', (req, res) => {
   }
 });
 
+router.post('/signup', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 module.exports = router;
