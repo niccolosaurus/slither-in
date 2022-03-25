971bd2b7fb527845f68e4737731f59cb7506834e
CarlosCastillo123 <ccastillo0098476@gmail.com>
2022-03-24 21:08:28 -0700

    removed logged in paramater in home routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -13,12 +13,12 @@ router.get('/', async (req, res) => {
                 },
             ],
         });
-
+        
         const animals = animalData.map((animal) => animal.get({ plain: true }));
-
+        
         res.render('homepage', {
             animals,
-            logged_in: req.session.logged_in
+        
         });
     } catch (err) {
         res.status(500).json(err);
