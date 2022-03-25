5c4d22fc78533aa60a6868e2b23a8580b75bb8de
Daniel Holland <danhol1989@gmail.com>
2022-03-24 19:49:09 -0700

    Starting up Dropzone

--- public/js/animal.js
+++ public/js/animal.js
@@ -28,3 +28,18 @@ const newAnimalHandler = async (event) => {
         }
     }
 }
+
+// Dropzone
+import Dropzone from "dropzone";
+// Optionally, import the dropzone file to get default styling.
+import "dropzone/dist/dropzone.css";
+import "./style.css";
+
+const myDropzone = new Dropzone("#my-form");
+
+const output = document.querySelector("#output");
+
+myDropzone.on("addedfile", (file) => {
+  // Add an info line about the added file for each file.
+  output.innerHTML += `<div>File added: ${file.name}</div>`;
+});
