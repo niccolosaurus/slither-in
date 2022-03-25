69ca34347187074ea02a68407a6bbec969614a55
CarlosCastillo123 <ccastillo0098476@gmail.com>
2022-03-24 18:31:35 -0700

    Fixed homeroutes.js added to animal.js main.handlebars

---
+++ public/js/animal.js
@@ -0,0 +1,35 @@
+const newAnimalHandler = async (event) => {
+    event.preventDefault();
+    const name = document.querySelector('#name').value.trim();
+    const type = document.querySelector('type').value.trim();
+    const species = document.querySelector('species').value.trim();
+    const sex = document.querySelector("sex")
+    const age = docuemnt.querySelector('age')
+    const pattern = document.querySelector('pattern')
+    const description = document.querySelector('description').value.trim
+    const gravid = document.querySelector('gravid')
+    const forSale = document.querySelector('forSale')
+    const deceased = document.querySelector('deceased')
+    const breeding = document.querySelector('breeding')
+
+    if (name && type && species && sex && age && pattern && description && gravid && forSale && deceased && breeding) {
+        const response = await fetch('/api/animals', {
+            method: 'POST',
+            body: JSON.stringify({ name, type, species, sex, age, pattern, description, gravid, forSale, deceased, breeding }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert('Failed to create project');
+        }
+    }
+}
+const maleParent = document.querySelector('maleParent')
+const femaleParent = document.querySelector('femaleParent')
+document
+    .querySelector('.new-project-form')
+    .addEventListener('submit', newFormHandler);
