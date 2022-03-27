b158d1c3a18fd45b0b829d9b7b16716e1e528ba8
CarlosCastillo123 <ccastillo0098476@gmail.com>
2022-03-26 21:11:28 -0700

    added delete js

---
+++ public/js/profile.js
@@ -0,0 +1,19 @@
+const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/animals/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to delete project');
+      }
+    }
+  };
+  
+  document
+  .getElementById('delete-button')
+  .addEventListener('click', delButtonHandler);
